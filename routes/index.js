f98d1850f34c58460094717485b2b1eee7d4de5d
DENNYYESSAR <dennisokeri5@gmail.com>
2024-10-02 08:29:54 -0400

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController.js';
+import UsersController from '../controllers/UsersController'; // Import the new UsersController
 
 const router = express.Router();
 
 // Existing endpoints
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
 
 // New endpoint for creating users
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
+router.post('/users', UsersController.postNew); // POST /users for creating a new user
 
 export default router;
